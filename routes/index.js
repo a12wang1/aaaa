8a81ca21ffe06a8f1506b02e40389a18873121f8
Sourire <a12wang@126.com>
2018-08-14 17:28:43 +0800

    modify ..

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   var config = req.app.get('config');  
   var web3 = new Web3();
   web3.setProvider(config.provider);
-  
+  var items = {};
   async.waterfall([
     function(callback) {
       web3.eth.getBlock("latest", false, function(err, result) {
@@ -18,13 +18,17 @@ router.get('/', function(req, res, next) {
       });
     }, function(lastBlock, callback) {
       var blocks = [];
-      
+      items.total = lastBlock.number;
       var blockCount = 10;
-      
+      if (items.total % blockCount ==0) {
+        items.totalPages = items.total /blockCount;
+      }else{
+        items.totalPages = items.total /blockCount +1;
+      }
       if (lastBlock.number - blockCount < 0) {
         blockCount = lastBlock.number + 1;
       }
-      
+      items.currentPage = 1;
       async.times(blockCount, function(n, next) {
         web3.eth.getBlock(lastBlock.number - n, true, function(err, block) {
           next(err, block);
@@ -47,7 +51,7 @@ router.get('/', function(req, res, next) {
         txs.push(tx);
       });
     });
-    res.render('index', { blocks: blocks, txs: txs });
+    res.render('index', { blocks: blocks, txs: txs , items: items});
   });
   
 });
